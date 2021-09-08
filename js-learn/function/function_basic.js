09612fa33294de9e3e6c005c917e807d357c196a
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-09-08 15:57:11 +0900

    commit the function basic1

---
+++ js-learn/function/function_basic.js
@@ -0,0 +1,15 @@
+// sample
+// use the arrow function!!
+const displayHello = () => {
+  console.log("Hello");
+  console.log("JavaScript")
+}
+
+displayHello();
+
+const culcTriangle = (bottom, height) => {
+  let area = bottom * height / 2;
+  return area;
+}
+
+console.log(culcTriangle(3,4));
