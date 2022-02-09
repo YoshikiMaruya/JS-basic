c59cbe95364724178e5f2aa743efbb47dc89b67a
YoshikiMaruya <yoshimaru0514@gmail.com>
2022-02-09 23:53:42 +0900

    js basic

---
+++ js-learn/basic/atcoder1.js
@@ -0,0 +1,3 @@
+let a = 2, b = 8;
+if(a * b % 2 == 0) console.log("even");
+else console.log("odd")
---
+++ js-learn/basic/atcoder2.js
@@ -0,0 +1,14 @@
+const a = "101";
+let count = 0;
+if(a[0]=='1'){
+    count++;
+}
+if(a[1]=='1'){
+    count++;
+}
+if(a[2]=='1'){
+    count++;
+}
+
+console.log(count)
+
